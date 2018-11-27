e3505291bd63ae9f777d4d2fc0e0a77a5122bbb5
Wiebke Frey <frey.wiebke@googlemail.com>
2018-11-27 17:43:59 +0100

    Finished basic bookmark incl. card placement

--- app/controllers/expos_controller.rb
+++ app/controllers/expos_controller.rb
@@ -4,4 +4,19 @@ class ExposController < ApplicationController
   def index
     @expos = Expo.all
   end
+
+  def bookmark
+    @expo = Expo.find(params[:id])
+    @bookmark = current_user.bookmarks.find_by(expo_id: @expo.id)
+
+    if @bookmark
+      @bookmark.destroy
+    else
+      Bookmark.create(expo_id: @expo.id, user_id: current_user.id)
+    end
+    respond_to do |format|
+      format.html { redirect_to expos_path }
+      format.js # <-- will render `app/views/expos/bookmark.js.erb`
+    end
+  end
 end
