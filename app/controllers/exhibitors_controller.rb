fd7dcdb7370f85ffb62862f66e7114912be788be
Sophie Rost <laram.risgen@gmail.com>
2018-11-29 18:41:43 +0100

    filtered pictures

--- app/controllers/exhibitors_controller.rb
+++ app/controllers/exhibitors_controller.rb
@@ -9,13 +9,12 @@ class ExhibitorsController < ApplicationController
     @exhibitor = Exhibitor.find(params[:id])
     @expo = Expo.find(params[:expo_id])
     @picture = Picture.new
-    @pictures = Picture.all
+    @pictures = user_pictures.reverse!
   end
 
   def favorite
     @exhibitor = Exhibitor.find(params[:id])
     @favorite = current_user.favorites.find_by(exhibitor_id: @exhibitor.id)
-
 
     if @favorite
       @favorite.destroy
@@ -26,5 +25,21 @@ class ExhibitorsController < ApplicationController
       format.html { redirect_to expo_exhibitors_path(@exhibitor.expo) }
       format.js # <-- will render `app/views/exhibitors/favorite.js.erb`
     end
+  end
+
+  private
+
+  def user_pictures
+    all_pictures = Picture.all
+    user = current_user.id
+    pictures_array = []
+
+    all_pictures.each do |picture|
+      if picture.user_id == user && picture.exhibitor_id == @exhibitor.id
+        pictures_array << picture
+      end
+    end
+    pictures_array.sort_by {:id}
+    return pictures_array
   end
 end
