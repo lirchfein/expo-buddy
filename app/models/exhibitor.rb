2144c62b62d07b6bd8673e1be8d5cea74801d1c1
Marton Barcza <marton.barcza@gmail.com>
2018-11-29 11:53:59 +0100

    initial search logic implemented

--- app/models/exhibitor.rb
+++ app/models/exhibitor.rb
@@ -3,4 +3,13 @@ class Exhibitor < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :pictures, dependent: :destroy
   has_many :users, through: :favorites
+
+  include PgSearch
+  pg_search_scope :search_by_name_and_description, against: {
+    :name => 'A',
+    :description => 'B'
+  },
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
