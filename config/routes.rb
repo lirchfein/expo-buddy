dcba96fa875087d5c43f4b57714123da3a98cfcf
Wiebke Frey <frey.wiebke@googlemail.com>
2018-12-03 16:03:00 +0100

    add basic pdf functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'expos/exhibitors/favorites', to: 'exhibitors#favorites_tab', as: 'favorites_tab'
   get 'expos/exhibitors/:id', to: 'exhibitors#favorite', as: 'favorite'
   get 'expos/exhibitors/update/:id', to: 'exhibitors#update', as: 'exhibitor_update'
+  # get 'expos/exhibitors/:id.pdf', to: 'exhibitors#show', as: 'pdf'
   devise_for :users
   resources :expos, only: %i[index show create] do
     resources :exhibitors, only: %i[index show create]
