ee3df6b43c10d3da1a2aba89f80595efc486c419
Sophie Rost <laram.risgen@gmail.com>
2018-12-04 14:12:06 +0100

    Add new note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,7 +2,16 @@ class NotesController < ApplicationController
   def create
     @exhibitor = Exhibitor.find(params[:exhibitor_id])
     @expo = @exhibitor.expo
-    @note = Note.create! params.require(:message).permit(:title, :content)
-    redirect_to message
+    @note = Note.new(note_params)
+    @note.user = current_user
+    @note.exhibitor = @exhibitor
+    @note.save
+    redirect_to expo_exhibitor_path(@expo, @exhibitor)
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:message, :exhibitor_id, :user_id)
   end
 end
