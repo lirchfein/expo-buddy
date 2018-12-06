55f944fe463ee0bf6965ceb074ad85fb1e020499
Paul Jelkmann <paul.jelkmann@gmail.com>
2018-12-06 11:53:51 +0100

    finished the ajax to toggle the show on the placeholder divs

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,4 +3,7 @@ class Note < ApplicationRecord
   belongs_to :user
 
   has_rich_text :content
+
+  scope :written_by, ->(a_user) { where(user: a_user) }
+  scope :for_exhibitor, ->(a_exhibitor) { where(exhibitor: a_exhibitor) }
 end
