91d08ded720def619ff120a11a1e4a2dd3d4dcf0
Wiebke Frey <frey.wiebke@googlemail.com>
2018-12-06 18:30:09 +0100

    first-about-page-skeletion-setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home]
+  skip_before_action :authenticate_user!, only: %i[home about]
+
+  def about
+  end
 
   def home
   end
